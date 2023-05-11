84928544591c35a5342aa5791cfc0d416189051c
Aayush Gupta <aayushgupta0403@gmail.com>
2023-05-11 17:42:29 +0530

    Add files via upload

---
+++ src/components/Products/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import "./Loader.scss";
+const Loader = () => {
+    
+    return (
+        <>
+      <div className="Load">
+        <div className="loader"></div>
+        <div className="text"> Products Loading please wait...</div>
+      </div>
+    
+       
+       </>
+    );
+};
+
+export default Loader;
